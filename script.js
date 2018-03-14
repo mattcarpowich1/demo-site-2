ad26d0ec167fe08381f41717e48a452099b8999c
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-03-13 19:47:27 -0700

    attempt 200

--- script.js
+++ script.js
@@ -1,73 +1,75 @@
-let once = false
-const nav = document.querySelector('nav')
-const title = document.querySelector('h4')
-const burger = document.querySelector('.burger')
-const sidebar = document.querySelector('aside')
-const closer = document.querySelector('.sidebar-close')
-const mid = document.querySelector('.mid-main')
-const main = document.querySelector('main')
+document.addEventListener('DOMContentLoaded', () => {
+  let once = false
+  const nav = document.querySelector('nav')
+  const title = document.querySelector('h4')
+  const burger = document.querySelector('.burger')
+  const sidebar = document.querySelector('aside')
+  const closer = document.querySelector('.sidebar-close')
+  const mid = document.querySelector('.mid-main')
+  const main = document.querySelector('main')
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  main.style.height = (screen.height) + 'px'
-  window.addEventListener('resize', () => {
-    main.style.height = screen.height + 'px'
-  })
-}
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 160) {
-    nav.classList.add('scrolly')
-    nav.classList.remove('hide')
-    title.classList.add('show')
-    // const children = Array.from(burger.children)
-    // children.forEach(node => node.classList.add('burger-row-white'))
-    if (!once) {
-      once = true
-    } 
-  } else {
-    if (once) {
-      nav.classList.add('hide')
-    }
-    // const children = Array.from(burger.children)
-    // children.forEach(node => node.classList.remove('burger-row-white'))
-    nav.classList.remove('scrolly')
-    title.classList.remove('show')
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    main.style.height = (screen.height) + 'px'
+    window.addEventListener('scroll', () => {
+      main.style.height = screen.height + 'px'
+    })
   }
-  // const yPos = -(window.pageYOffset / mid.dataset.speed)
-  // const coords = 'calc(50% + ' + yPos + 'px) 40%'
-  // mid.style.backgroundPosition = coords
-})
 
-burger.addEventListener('click', () => {
-  sidebar.classList.add('sidebar-open')
-  closer.classList.add('sidebar-close-active')
-})
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 160) {
+      nav.classList.add('scrolly')
+      nav.classList.remove('hide')
+      title.classList.add('show')
+      // const children = Array.from(burger.children)
+      // children.forEach(node => node.classList.add('burger-row-white'))
+      if (!once) {
+        once = true
+      } 
+    } else {
+      if (once) {
+        nav.classList.add('hide')
+      }
+      // const children = Array.from(burger.children)
+      // children.forEach(node => node.classList.remove('burger-row-white'))
+      nav.classList.remove('scrolly')
+      title.classList.remove('show')
+    }
+    // const yPos = -(window.pageYOffset / mid.dataset.speed)
+    // const coords = 'calc(50% + ' + yPos + 'px) 40%'
+    // mid.style.backgroundPosition = coords
+  })
 
-closer.addEventListener('click', () => {
-  sidebar.classList.remove('sidebar-open')
-  closer.classList.remove('sidebar-close-active')
-})
+  burger.addEventListener('click', () => {
+    sidebar.classList.add('sidebar-open')
+    closer.classList.add('sidebar-close-active')
+  })
 
-// window.addEventListener('resize', () => {
-//   console.log(window.innerHeight)
-//   main.style.height = window.innerHeight + 'px'
-// })
+  closer.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar-open')
+    closer.classList.remove('sidebar-close-active')
+  })
 
-// $('header').each(function(){
-//     var $bgobj = $(this); // assigning the object
+  // window.addEventListener('resize', () => {
+  //   console.log(window.innerHeight)
+  //   main.style.height = window.innerHeight + 'px'
+  // })
 
-//     $(window).scroll(function() {
+  // $('header').each(function(){
+  //     var $bgobj = $(this); // assigning the object
 
-//       // Scroll the background at var speed
-//       // the yPos is a negative value because we're scrolling it UP!                              
-//       var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+  //     $(window).scroll(function() {
 
-//       // Put together our final background position
-//       var coords = '50% '+ yPos + 'px';
+  //       // Scroll the background at var speed
+  //       // the yPos is a negative value because we're scrolling it UP!                              
+  //       var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
 
-//       // Move the background
-//       $bgobj.css({ backgroundPosition: coords });
+  //       // Put together our final background position
+  //       var coords = '50% '+ yPos + 'px';
 
-//     });
+  //       // Move the background
+  //       $bgobj.css({ backgroundPosition: coords });
 
-//   });
+  //     });
+
+  //   });
+}) 
